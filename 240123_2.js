d8deb4cf1be783a518676bf1765525d823202a0a
jeju-circle <hyunseo1018@naver.com>
2024-01-23 21:44:54 +0900

    240123_4

--- 240123_2.js
+++ 240123_2.js
@@ -1,12 +1,6 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    const clickedclass = "clicked";
-    if (h1.classList.contains(clickedclass)) {
-        h1.classList.remove(clickedclass);
-    } else {
-        h1.classList.add(clickedclass);
-    }
+    h1.classList.toggle("clicked");
 }
-
 h1.addEventListener("click", handleTitleClick);
