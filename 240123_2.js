9a3fb082e83674f615aa8dcd391fc2ed08cdcf79
jeju-circle <hyunseo1018@naver.com>
2024-03-26 21:43:19 +0900

    commit_240326

--- 240123_2.js
+++ 240123_2.js
@@ -1,10 +1,15 @@
-const loginform = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const Loginform = document.querySelector("#login-form");
+const LoginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    Loginform.classList.add(HIDDEN_CLASSNAME);
+    const username = LoginInput.value;
+    greeting.innerText = `hello ${username}`; //백틱을 이용할것! string 과 변수를 결합할 수 있게 해줌 (변수를 string 안에 포함시키는 방법 = ` ${변수명}` )
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginform.addEventListener("submit", onLoginSubmit);
+Loginform.addEventListener("submit", onLoginSubmit);
